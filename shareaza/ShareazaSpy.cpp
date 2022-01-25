339fc9538b164ade88c01f2bb00ca2ff657c043f
Fabio Melo Pfeifer <fmpfeifer@gmail.com>
2022-01-25 09:07:17 -0300

    Fix include

--- shareaza/ShareazaSpy.cpp
+++ shareaza/ShareazaSpy.cpp
@@ -1,3 +1,4 @@
+#include "StdAfx.h"
 #include "ShareazaSpy.h"
 #include "Shareaza.h"
 #include "QuerySearch.h"
--- shareaza/ShareazaSpy.h
+++ shareaza/ShareazaSpy.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "StdAfx.h"
-
 #define SHAREAZA_SPY_OUTPUT_FOLDER "C:\\ShareazaSpyTemp"
 
 void LogReceivedPackage(SOCKADDR_IN* addr);
